7bbc4a2ff0bcd6386f2860b47cc4d5a39d1e19d7
Swastika0015 <swastikayadav5358@gmail.com>
2022-03-23 02:43:20 +0530

    Update mainpagecard.jsx

--- src/components/mainpagecard/mainpagecard.jsx
+++ src/components/mainpagecard/mainpagecard.jsx
@@ -9,14 +9,14 @@ const Mainpagecard = () => {
             <div className="card-container">
                 <div className="flex-flow-row card-items">
                     <div className="card card-component">
-                        <img className="card-img" src={pic}/>
+                        <img className="card-img" src={pic} alt="women"/>
                         <div className="card-details flex-flow-column">
                             <h2><a>Apparel</a></h2> 
                         </div>
                     </div>
 
                     <div className="card card-component">
-                        <img className="card-img" src={pic}/>
+                        <img className="card-img" src={pic} alt="women"/>
                         <span className="bestseller-badge"><h3>Flat 40% off</h3></span>
                         <div className="card-details flex-flow-column">
                             <h2><a>Flash Sale</a></h2>
@@ -24,7 +24,7 @@ const Mainpagecard = () => {
                     </div>
 
                     <div className="card card-component">
-                        <img className="card-img" src={pic}/>
+                        <img className="card-img" src={pic} alt="women"/>
                         <div className="card-details flex-flow-column">
                             <h2><a>Beauty</a></h2>
                         </div>  
